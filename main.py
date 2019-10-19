59e19b99b1139d78434d7c0525bab49d88af24a9
Rickard Englund <rickard.englund@skf.com>
2019-10-19 20:45:22 +0200

    speed up

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ if __name__ == '__main__':
             dist = distance()
             print(dist)
             something_there = something_there_func(dist)
-            time.sleep(0.5)
+            # time.sleep(0.1)
             dist = distance()
             print(dist)
             something_there &= something_there_func(distance())
